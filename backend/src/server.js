8c2593760c3274600ca3802e31f0b897407418e8
gpcoding15 <gisela.pardini15@gmail.com>
2026-09-17 22:08:15 -0300

    Add client creation endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,12 +15,26 @@ const clients = [
   },
 ];
 
-app.get("/clients", (request, response) => {
-    response.json(clients)
-})
+app.use(express.json());
 
 app.get("/", (request, response) => {
     response.send("Cobralo")
+});
+
+app.get("/clients", (request, response) => {
+    response.json(clients)
+});
+
+app.post("/clients", (request, response) => {
+    const new_client = {
+        id: clients.length + 1,
+        name: request.body.name,
+        email: request.body.email
+    };
+
+    clients.push(new_client);
+
+    response.status(201).json(new_client);
 });
 
 app.listen(3000, () => {
